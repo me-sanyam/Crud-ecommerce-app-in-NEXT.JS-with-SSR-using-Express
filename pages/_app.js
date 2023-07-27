2c143cadb165e959815ffb9821f651af3a855037
sanyam kapoor <sanyamkapoor001@gmail.com>
2023-07-27 16:26:36 +0530

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
